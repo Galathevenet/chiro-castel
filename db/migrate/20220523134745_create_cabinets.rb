aacc7ec27a42acf2b10925f7c540916ab8b748e6
Gala Thevenet <galathevenet@hotmail.com>
2022-05-23 15:49:38 +0200

    created Cabinet model with address long and lat for geocoder and mapbox

---
+++ db/migrate/20220523134745_create_cabinets.rb
@@ -0,0 +1,11 @@
+class CreateCabinets < ActiveRecord::Migration[6.1]
+  def change
+    create_table :cabinets do |t|
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
