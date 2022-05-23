8961dfbfd6ea9e68e11e2d336d66db6296d3afec
Gala Thevenet <galathevenet@hotmail.com>
2022-05-23 15:57:58 +0200

    added methods to controller + geocoder to model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
+    @cabinets = Cabinet.all
+    @markers = @cabinets.geocoded.map do |cabinet|
+      {
+        lat: cabinet.latitude,
+        lng: cabinet.longitude
+      }
+    end
   end
 end
